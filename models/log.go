fc4cf21e24aafb1f1f77d5491da4e8f5b8e8cbe7
ntquang98 <ntquang17021998@gmail.com>
2025-07-21 15:29:06 +0700

    update

--- models/log.go
+++ models/log.go
@@ -9,6 +9,8 @@ import (
 type Log struct {
 	ID              primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	Type            string             `json:"type,omitempty" bson:"type,omitempty"`
+	Method          string             `json:"method,omitempty" bson:"method,omitempty"`
+	URL             string             `json:"url,omitempty" bson:"url,omitempty"`
 	StoreDomain     string             `json:"storeDomain,omitempty" bson:"store_domain,omitempty"`
 	ScriptLoaded    bool               `json:"scriptLoaded,omitempty" bson:"script_loaded,omitempty"`
 	CustomilyLoaded bool               `json:"customilyLoaded,omitempty" bson:"customily_loaded,omitempty"`
